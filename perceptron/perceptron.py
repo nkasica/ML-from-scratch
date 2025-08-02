c4a03a1ce6bca6cf326577361d36cf6eb20802e1
Noah Kasica <nkasica21@gmail.com>
2025-08-02 16:12:53 -0400

    simple perceptron implementation - need to add some comments - maybe change testing

---
+++ perceptron/perceptron.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class Perceptron:
+
+    def __init__(self, d):
+        self.weights = np.random.uniform(-1, 1, d)
+        self.bias = 0
+
+    def train(self, X: np.ndarray, Y: np.ndarray, iter: int):
+        for _ in range(iter):
+            for x, y in zip(X, Y): 
+                a = x @ self.weights + self.bias
+
+                if y * a <= 0:
+                    self.weights += y * x
+                    self.bias += y 
+
+
+    def test(self, xhat: np.ndarray):
+        return np.sign(xhat @ self.weights + self.bias)
